daadc559b0c89c4f3f79ffedfbce9623d84c9f73
JosePanero <92395880+JosePanero@users.noreply.github.com>
2024-09-24 16:42:53 -0300

    Feature swt 15 websockets (#2)
    * add class, exceptions, router, test
    
    * fix some issues
    
    * fix some imports and tests
    
    * remove temporal endpoint
    
    * remove temporal endpoint
    
    ---------
    
    Co-authored-by: Jose Panero <junxior2000@gmail.com>

--- app/main.py
+++ app/main.py
@@ -1,8 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket
+from app.routers import matches
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ["*"]
+app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+app.include_router(matches.router)
 
 @app.get("/")
 def hello_world():
     return {"Hello": "World"}
-
---
+++ app/routers/matches.py
@@ -0,0 +1,32 @@
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect, WebSocketException
+
+from app.exceptions import *
+from app.connection_manager import ConnectionManager
+
+router = APIRouter(prefix="/matches")
+
+manager = ConnectionManager()
+
+@router.websocket("/{game_id}/ws/{player_id}")
+async def create_websocket_connection(game_id: int, player_id: int, websocket: WebSocket):
+    await websocket.accept()
+    try:
+        try:
+            manager.connect_player_to_game(game_id, player_id, websocket)
+            await manager.keep_alive(websocket)
+        except GameConnectionDoesNotExist:
+            await websocket.send_json(
+                {"Error": f"Conexión a la partida {game_id} no existe"}
+            )
+            await websocket.close()
+        except PlayerAlreadyConnected:
+            await websocket.send_json(
+                {
+                    "Error": f"Jugador {player_id} ya tiene una conexión activa a la partida {game_id}"
+                }
+            )
+            await websocket.close()
+            raise WebSocketDisconnect
+
+    except WebSocketDisconnect:
+        manager.disconnect_player_from_game(game_id, player_id)
