0b877b2fa54ab43177a55dc880f68ac9698b7907
Jose Panero <junxior2000@gmail.com>
2024-09-24 15:38:30 -0300

    remove temporal endpoint

--- app/main.py
+++ app/main.py
@@ -18,15 +18,3 @@ app.include_router(matches.router)
 @app.get("/")
 def hello_world():
     return {"Hello": "World"}
-
-@app.websocket("/ws/")
-async def create_websocket_connection(websocket: WebSocket):
-    await websocket.accept()
-
-    await websocket.send_json({"msg": "Hello WebSocket"})
-    
-    while True:
-        data = await websocket.receive_text()
-        await websocket.send_text(
-            f"Session cookie or query token value is"
-        )
