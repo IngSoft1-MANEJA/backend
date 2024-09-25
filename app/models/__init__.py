0136e4243b702763754addc823b8608f1ec3fcb8
JosePanero <92395880+JosePanero@users.noreply.github.com>
2024-09-25 15:12:28 -0300

    Revert "Feature swt 25 create database models (#3)"
    This reverts commit b1b2c8e98ec91aa56fd8e2a1b6007778fb16d0b9.

--- app/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# File that initializes the models package and imports the models
-
-from .models import Matches, Players, Boards, Tiles, ShapeCards, MovementCards
-from .enums import Colors, Shapes, Movements
-
