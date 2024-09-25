e503f50cc6ed26bf65bbcd62b274a22c76065cc1
Ezequiel Marin <ezequiel.marin@mi.unc.edu.ar>
2024-09-25 15:28:48 -0300

    Feature swt-25 create database models (#5)
    * Adding all models of the database
    
    * Correction to configuration for initialize the database
    
    * Adding test for the models
    
    * Adding pytest.ini file for configurate pytest env
    
    * Correction database models and adding requeriments

--- app/database.py
+++ app/database.py
@@ -1,14 +1,19 @@
 from sqlalchemy import create_engine, Column, Integer, String, Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from config import DATABASE_FILENAME
+
+from app.config import DATABASE_FILENAME
 
 
 # Configuración de la base de datos
 engine = create_engine(f'sqlite:///{DATABASE_FILENAME}', echo=True)
+Base = declarative_base()
 
 # Crea una sesión
-Session = sessionmaker(bind=engine)
-session = Session()
+Init_Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+run_session = Init_Session()
 
-Base = declarative_base()
+def init_db():
+    import app.models
+    Base.metadata.create_all(bind=engine)
+    print("Database initialized")
---
+++ app/models/__init__.py
@@ -0,0 +1,5 @@
+# File that initializes the models package and imports the models
+
+from .models import Matches, Players, Boards, Tiles, ShapeCards, MovementCards
+from .enums import Colors, Shapes, Movements
+
