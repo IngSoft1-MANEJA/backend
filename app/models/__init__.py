9d05e66c9be69316c7d6854ee65fd1a32f296a21
Ezequiel Marin <marinreynaezequiel@gmail.com>
2024-09-30 19:26:46 -0300

    Revert "Feature swt 10 create endpoint abandone match (#9)"
    This reverts commit 16b7388d79c92f9670b7c3df8e8ef4707a4c9410.

--- app/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# File that initializes the models package and imports the models
-
-from app.models.models import Matches, Players, Boards, Tiles, ShapeCards, MovementCards
-from app.models.enums import Colors, Shapes, Movements
-
